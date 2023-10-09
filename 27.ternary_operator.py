1308fc49e354131eb2edadc6765256c2dd32cd97
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-09 16:14:15 +0545

    2nd day

---
+++ 27.ternary_operator.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+
+output = "Even number" if number % 2 == 0 else "Odd number"
+print(output)
+
+print("Even number" if number % 2 == 0 else "Odd number")
---
+++ 28.loops.py
@@ -0,0 +1,10 @@
+# The loops are
+# For loops
+
+#for in range(inclusive,exclusive,step):
+#code
+
+# While loops
+
+for i in range(1, 5):
+    print("Govind Kumar Yadav")
