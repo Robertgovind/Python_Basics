28a9033a7e44611f7fc54940567b12091f67d6ce
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-09 16:28:51 +0545

    2nd day

--- 28.loops.py
+++ 28.loops.py
@@ -1,10 +1,17 @@
 # The loops are
 # For loops
 
-#for in range(inclusive,exclusive,step):
-#code
+# for in range(inclusive,exclusive,step):
+# code
 
 # While loops
+# while condition:
+# code
 
 for i in range(1, 5):
     print("Govind Kumar Yadav")
+
+i = 0
+while i <= 10:
+    print("Govind kumar yaddav")
+    i += 1
---
+++ 29.sequence_in_list.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "mango", "Pineapple", "banana", "orange"]
+
+for i in fruits:
+    print(i)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in numbers:
+    print(i)
